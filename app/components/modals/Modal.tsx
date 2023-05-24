8ec4cf12de77e1476b625d1c479fb73244d56c05
Salahuddin-Gazi <gazisalahuddin@outlook.com>
2023-05-24 12:21:06 +0600

    Undone the styels css

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -64,7 +64,7 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       {/* justify-center items-center flex */}
-      <div className="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 flex justify-center items-center">
+      <div className="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
           {/* Content */}
           <div
